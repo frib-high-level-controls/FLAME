5579f859bbb8c53af81dd9f4a09c13aa7f11aa80
Johan Bengtsson <jbngtssn@gmail.com>
2015-08-19 14:18:48 -0400

    Tweaked cav_hom.py.

--- python/cavity_model/cav_hom.py
+++ python/cavity_model/cav_hom.py
@@ -1,20 +1,35 @@
 import math
 import numpy
-import sys
-import time
+import scipy.integrate
 
 home_dir = '/home/bengtsson/FRIB/Cavity Model/Multipole41/'
 
 
 def rd_hom(file_name):
-    lines = [line.rstrip('\n') for line in open(file_name)]
-    for line in lines:
-        print '%s' % (line)
+
+    xy = numpy.loadtxt(file_name)
+    xy = numpy.array(xy)
+    # z-axis is in [mm].
+    xy[:,0] *= 1e-3
+    return xy
 
 
-#def integrate_hom():
+def integrate_hom(file_name):
+    xy = rd_hom(file_name)
+    return {
+        'simps': scipy.integrate.simps(xy[:,1], xy[:,0], even='avg'),
+        'trapz': numpy.trapz(xy[:,1], xy[:,0])
+        }['simps']
 
 
-file_name = 'CaviMlp_EDipole_41.txt'
+Edip = integrate_hom(home_dir+'CaviMlp_EDipole_41.txt')
+print 'Edip    = %11.4e [MV]' % (1e-6*Edip)
 
-rd_hom(home_dir+file_name)
+Equad = integrate_hom(home_dir+'CaviMlp_EQuad_41.txt')
+print 'Equad   = %11.4e [MV/m]' % (1e-6*Equad)
+
+Efocus1 = integrate_hom(home_dir+'CaviMlp_EFocus1_41.txt')
+print 'Efocus1 = %11.4e [MV/m]' % (1e-6*Efocus1)
+
+Efocus2 = integrate_hom(home_dir+'CaviMlp_EFocus2_41.txt')
+print 'Efocus2 = %11.4e [MV/m]' % (1e-6*Efocus2)
