516f42d06f7db0ea39469e10fb296aff341ee56e
Johan Bengtsson <jbngtssn@gmail.com>
2015-08-19 15:29:40 -0400

    Tweaked cav_hom.py.

--- python/cavity_model/cav_hom.py
+++ python/cavity_model/cav_hom.py
@@ -8,7 +8,6 @@ home_dir = '/home/bengtsson/FRIB/Cavity Model/Multipole41/'
 def rd_hom(file_name):
 
     xy = numpy.loadtxt(file_name)
-    xy = numpy.array(xy)
     # z-axis is in [mm].
     xy[:,0] *= 1e-3
     return xy
@@ -16,20 +15,32 @@ def rd_hom(file_name):
 
 def integrate_hom(file_name):
     xy = rd_hom(file_name)
-    return {
-        'simps': scipy.integrate.simps(xy[:,1], xy[:,0], even='avg'),
-        'trapz': numpy.trapz(xy[:,1], xy[:,0])
-        }['simps']
+
+    method = 'simps'
+    if method == 'simps':
+        integ = scipy.integrate.simps(xy[:,1], xy[:,0], even='avg')
+    elif method == 'trapz':
+        integ = numpy.trapz(xy[:,1], xy[:,0])
+    elif method == 'trapz':
+        # Romberg's method requires 2^k + 1 samples.
+        integ = scipy.integrate.romb(xy[:,1], xy[:,0])
+    return integ
 
 
+print
 Edip = integrate_hom(home_dir+'CaviMlp_EDipole_41.txt')
-print 'Edip    = %11.4e [MV]' % (1e-6*Edip)
-
 Equad = integrate_hom(home_dir+'CaviMlp_EQuad_41.txt')
-print 'Equad   = %11.4e [MV/m]' % (1e-6*Equad)
-
 Efocus1 = integrate_hom(home_dir+'CaviMlp_EFocus1_41.txt')
-print 'Efocus1 = %11.4e [MV/m]' % (1e-6*Efocus1)
-
 Efocus2 = integrate_hom(home_dir+'CaviMlp_EFocus2_41.txt')
-print 'Efocus2 = %11.4e [MV/m]' % (1e-6*Efocus2)
+
+Hdip = integrate_hom(home_dir+'CaviMlp_HDipole_41.txt')
+Hmono = integrate_hom(home_dir+'CaviMlp_HMono_41.txt')
+Hquad = integrate_hom(home_dir+'CaviMlp_HQuad_41.txt')
+print
+print 'Efocus1 = %13.10f [MV/m]' % (1e-6*Efocus1)
+print 'Edip    = %13.10f [MV]' % (1e-6*Edip)
+print 'Hdip    = %13.10f [MA]' % (1e-6*Hdip)
+print 'Hmono   = %13.10f [MA/m]' % (1e-6*Hmono)
+print 'Hquad   = %13.10f [MA/m]' % (1e-6*Hquad)
+print 'Equad   = %13.10f [MV/m]' % (1e-6*Equad)
+print 'Efocus2 = %13.10f [MV/m]' % (1e-6*Efocus2)
