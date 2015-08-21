179c6ad283e3ebc4b4a5581c55b3b5b4bc6684c6
Johan Bengtsson <jbngtssn@gmail.com>
2015-08-20 23:19:19 -0400

    Tweaked cav_hom.py.

--- python/cavity_model/cav_hom.py
+++ python/cavity_model/cav_hom.py
@@ -15,22 +15,11 @@ def rd_hom(file_name):
     return xy
 
 
-def triang_smooth(y, x):
-    n = len(x)
-    integ = 0.0
-    for k in range(0, n-1, 1):
-        integ += (y[k+1]-y[k])*(x[k+1]-x[k])
-        if abs(y[k+1]) > abs(y[k]):
-            integ += y[k]*(x[k+1]-x[k])
-        else:
-            integ += y[k+1]*(x[k+1]-x[k])
-    return integ
-
-
 def integrate_hom(file_name):
     xy = rd_hom(file_name)
+    print len(xy[:, 0])
 
-    method = 'triang_smooth'
+    method = 'trapz'
     if method == 'trapz':
         integ = numpy.trapz(xy[:, 1], xy[:, 0])
     elif method == 'simps':
@@ -51,8 +40,6 @@ def integrate_hom(file_name):
 #        print '%3d %3d %3d %1d %3d %7.5f' % \
             (n+npad, len(xy[:, 0]), n, k, npad, h)
         integ = scipy.integrate.romb(xy[:, 1], dx=h)
-    elif method == 'triang_smooth':
-        integ = triang_smooth(xy[:, 1], xy[:, 0])
     return integ
 
 
@@ -65,10 +52,10 @@ Hdip    = integrate_hom(home_dir+'CaviMlp_HDipole_41.txt')
 Hmono   = integrate_hom(home_dir+'CaviMlp_HMono_41.txt')
 Hquad   = integrate_hom(home_dir+'CaviMlp_HQuad_41.txt')
 print
-print 'Efocus2 = %13.10f [MV/m]' % (1e-6*Efocus2)
-print 'Edip    = %13.10f [MV]'   % (1e-6*Edip)
-print 'Hdip    = %13.10f [MA]'   % (1e-6*Hdip)
-print 'Hmono   = %13.10f [MA/m]' % (1e-6*Hmono)
-print 'Hquad   = %13.10f [MA/m]' % (1e-6*Hquad)
-print 'Equad   = %13.10f [MV/m]' % (1e-6*Equad)
-print 'Efocus1 = %13.10f [MV/m]' % (1e-6*Efocus1)
+print 'Efocus2 = %18.15f [MV/m]' % (1e-6*Efocus2)
+print 'Edip    = %18.15f [MV]'   % (1e-6*Edip)
+print 'Hdip    = %18.15f [MA]'   % (1e-6*Hdip)
+print 'Hmono   = %18.15f [MA/m]' % (1e-6*Hmono)
+print 'Hquad   = %18.15f [MA/m]' % (1e-6*Hquad)
+print 'Equad   = %18.15f [MV/m]' % (1e-6*Equad)
+print 'Efocus1 = %18.15f [MV/m]' % (1e-6*Efocus1)
