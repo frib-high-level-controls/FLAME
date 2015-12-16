6c126b72a3d853940dcc305184dd96d8bd2b64a7
Michael Davidsaver <mdavidsaver@gmail.com>
2015-12-16 11:00:43 -0500

    start test_moment

---
+++ python/uscsi/test/test_moment.py
@@ -0,0 +1,34 @@
+
+import unittest
+import numpy
+from numpy import testing as NT
+from numpy.testing import assert_array_almost_equal_nulp as assert_aequal
+
+from .. import Machine
+
+class testBasic(unittest.TestCase):
+  def setUp(self):
+    T = self.expect = numpy.asfarray([
+      [1, 0, 1, 0, 1, 0],
+      [0, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0],
+      [0, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0],
+      [0, 1, 0, 1, 0, 1],
+    ])
+    self.M = Machine({
+      'sim_type':'MomentMatrix',
+      'elements':[
+        {'name':'elem0', 'type':'source', 'initial':T},
+        {'name':'elem1', 'type':'generic', 'transfer':numpy.identity(6)},
+      ],
+    })
+
+  def test_generic(self):
+    "Propogate a state matrix through a generic section"
+
+    S = self.M.allocState({})
+
+    self.M.propagate(S)
+
+    assert_aequal(S.state, self.expect)
