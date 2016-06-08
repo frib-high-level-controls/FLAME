fac596c30dcc66dcef8667022f4b828eee3409f3
jbengtsson <jbngtssn@gmail.com>
2016-06-08 15:59:58 -0400

    Disable instrumented code for debugging in rf_cavity.cpp.

--- src/rf_cavity.cpp
+++ src/rf_cavity.cpp
@@ -253,10 +253,10 @@ void ElementRFCavity::TransFacts(const int cavilabel, double beta, const double 
     std::ostringstream  strm;
 
     // For debugging of TTF function.
-    if (true) {
+    if (false) {
         calTransfac(inf2, 2, CaviIonK, true, Ecen, T, Tp, S, Sp, V0);
         V0 *= EfieldScl;
-//        printf("\n%19.12e %19.12e %19.12e %19.12e %19.12e %19.12e\n", Ecen, T, Tp, S, Sp, V0);
+        printf("\n%19.12e %19.12e %19.12e %19.12e %19.12e %19.12e\n", Ecen, T, Tp, S, Sp, V0);
 //        return;
     }
 
